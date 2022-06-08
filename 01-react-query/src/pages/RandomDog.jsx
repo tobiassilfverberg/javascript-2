2ac261e37c1b6cf522e261f3fe138fbea9796567
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-06-08 14:04:04 +0200

    Fixed styling for image showing dog.

--- 01-react-query/src/pages/RandomDog.jsx
+++ 01-react-query/src/pages/RandomDog.jsx
@@ -6,14 +6,14 @@ const RandomDog = () => {
 	const { isLoading, isError, error, data } = useQuery("random-dog", getRandomDog)
 
   return (
-    <Container>
+    <Container className="vw-80 vh-80">
 		<h1>Random dogs for everyone</h1>
 
 		{isLoading && (<p>Random dog is loading...</p>)}
 
 		{isError && (<p>An error occured: {error.message}</p>)}
 
-		{data && (<img src={data.message} />)}
+		{data && (<img className="img-fluid" src={data.message} />)}
 
 	</Container>
   )
