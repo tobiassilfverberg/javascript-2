f89b7a792031ae4237e1c5f2a30e71634556de8c
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-06-08 11:46:54 +0200

    Implemeted react-query in randomdadjokepage.

--- 01-react-query/src/main.jsx
+++ 01-react-query/src/main.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<BrowserRouter> 
-			<App />
-		</BrowserRouter>
+
+		<QueryClientProvider client={queryClient}> 
+			<BrowserRouter> 
+				<App />
+			</BrowserRouter>
+		</QueryClientProvider>
+		
 	</React.StrictMode>
 )
--- 01-react-query/src/pages/RandomDadJokePage.jsx
+++ 01-react-query/src/pages/RandomDadJokePage.jsx
@@ -1,9 +1,22 @@
 import Container from 'react-bootstrap/Container'
+import { useQuery } from 'react-query'
 import { getRandomDadJoke } from '../services/ICanHazDadJokeAPI'
 
 const RandomDadJokePage = () => {
+	const { isLoading, isError, error, data } = useQuery('random-dad-joke', getRandomDadJoke)
+
 	return (
-		<Container className="py-3">RandomDadJokePage</Container>
+		<Container className="py-3">
+
+			<h1>Random dad joke</h1>
+
+			{isLoading && (<p>Loading dad joke...</p>)}
+
+			{isError && (<p>An error occoured: {error.message}</p>)}
+
+			{data && (<p> {data.joke} </p> )}
+			
+		</Container>
 	)
 }
 
