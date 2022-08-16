d187d5c2dd372856e55efd0549ccea098816f0c6
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-08-16 11:58:58 +0200

    Added queryClient

--- 05-books-library/src/main.jsx
+++ 05-books-library/src/main.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider} from 'react-query'
 import App from './App'
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+			staleTime: 1000 * 60 * 60, // 1 hour
+			cacheTime: 1000 * 60 * 60, // 1 hour
+		}
+	}
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</QueryClientProvider>
 	</React.StrictMode>
 )
