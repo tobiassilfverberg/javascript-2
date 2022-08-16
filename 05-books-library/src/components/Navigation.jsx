09b0fbc2531af7fc20ff46c4e1e3b7db973ef958
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-08-16 13:50:08 +0200

    Now display how many books I have.

--- 05-books-library/src/components/Navigation.jsx
+++ 05-books-library/src/components/Navigation.jsx
@@ -2,8 +2,11 @@ import Container from 'react-bootstrap/Container'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import { Link, NavLink } from 'react-router-dom'
+import useBooks from '../hooks/useBooks'
 
 const Navigation = () => {
+	const { data: books } = useBooks()
+
 	return (
 		<Navbar bg="dark" variant="dark" expand="md">
 			<Container>
@@ -12,7 +15,14 @@ const Navigation = () => {
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="ms-auto">
-						<Nav.Link as={NavLink} end to="/books">Books</Nav.Link>
+						<Nav.Link as={NavLink} end to="/books">
+						Books&nbsp;
+							{
+								books
+									? `(${books.length})`
+									: ''
+							}
+						</Nav.Link>
 						<Nav.Link as={NavLink} end to="/authors">Authors</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
