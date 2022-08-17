f628ae688d6048875997cbc6971fb3739078d30a
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-08-17 11:39:53 +0200

    Now render authors using the BasicTable React Table component.

--- 05-books-library/src/pages/AuthorsPage.jsx
+++ 05-books-library/src/pages/AuthorsPage.jsx
@@ -1,11 +1,25 @@
+import { useMemo } from 'react'
 import Container from 'react-bootstrap/Container'
 import LoadingSpinner from '../components/LoadingSpinner'
 import WarningAlert from '../components/alerts/WarningAlert'
-import AuthorList from '../components/AuthorList'
+import BasicTable from '../components/BasicTable'
 import useAuthors from '../hooks/useAuthors'
 
 const AuthorsPage = () => {
 	const { data: authors, error, isError, isLoading } = useAuthors()
+
+	const columns = useMemo(() => {
+		return [
+			{
+				Header: 'Name of author', 
+				accessor: 'name'
+			},
+			{
+				Header: 'Date of Birth', 
+				accessor: 'date_of_birth'
+			},
+		]
+	}, [])
 
 	return (
 		<Container className="py-3">
@@ -15,7 +29,7 @@ const AuthorsPage = () => {
 
 			{isError && <WarningAlert message={error.message} />}
 
-			{authors && <AuthorList authors={authors} />}
+			{authors && <BasicTable columns={columns} data={authors} />}
 		</Container>
 	)
 }
