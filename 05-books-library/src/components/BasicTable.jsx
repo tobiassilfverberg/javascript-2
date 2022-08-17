3333713b0c366913571a8148a19894c44e7de4f9
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-08-17 11:12:25 +0200

    Finished BasicTable component so now the app is using React Table to render all the books with info.

--- 05-books-library/src/components/BasicTable.jsx
+++ 05-books-library/src/components/BasicTable.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import { useTable } from 'react-table'
+import Table from 'react-bootstrap/Table'
 
 const BasicTable = ({ columns, data }) => {
+	const { 
+		getTableProps,
+		getTableBodyProps,
+		headerGroups,
+		rows,
+		prepareRow,
+	} = useTable({ columns, data })
+
 	return (
-		<div>I am a BasicTable</div>
+		<Table {...getTableProps()}>
+			<thead>
+				{headerGroups.map(headerGroup => (
+					<tr {...headerGroup.getHeaderGroupProps()}>
+						{headerGroup.headers.map(column => (
+							<th {...column.getHeaderProps()}>
+								{column.render('Header')}
+							</th>
+						))}
+					</tr>
+				))}
+			</thead>
+
+			<tbody {...getTableBodyProps()}>
+				{rows.map(row => {
+					prepareRow(row)
+					return (
+						<tr {...row.getRowProps()}>
+							{row.cells.map(cell => (
+								<td {...cell.getCellProps()}>
+									{cell.render('Cell')}
+								</td>
+							))}
+						</tr>
+					)
+				})}
+			</tbody>
+		</Table>
 	)
 }
 
