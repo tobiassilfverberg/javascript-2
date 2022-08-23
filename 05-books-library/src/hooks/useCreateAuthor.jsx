e91773035f0301e7bbd2e3daf2757044fb1b3693
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-08-23 12:16:09 +0200

    Added toasts depending on 'error' or 'success'.

--- 05-books-library/src/hooks/useCreateAuthor.jsx
+++ 05-books-library/src/hooks/useCreateAuthor.jsx
@@ -1,11 +1,22 @@
 import { useMutation, useQueryClient } from 'react-query'
+import { toast } from 'react-toastify'
 import BooksAPI from '../services/BooksAPI'
 
 const useCreateAuthor = () => {
 	const queryClient = useQueryClient()
 
 	return useMutation(BooksAPI.createAuthor, {
+		onError: (error) => {
+			toast.warning(
+				<>
+					<strong>Something bad happened</strong> <br />
+					{error?.message}
+				</>
+			)
+		}, 
+
 		onSuccess: () => {
+			toast.success("Author created!")
 			// invalidate data
 			queryClient.invalidateQueries("authors")
 		}
