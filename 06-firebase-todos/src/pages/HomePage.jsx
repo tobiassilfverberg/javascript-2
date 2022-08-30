23f3f1ec6195bec5d985b3f57d25ae56544b3f91
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-08-30 13:49:48 +0200

    Use AddTodoForm.

--- 06-firebase-todos/src/pages/HomePage.jsx
+++ 06-firebase-todos/src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import Container from 'react-bootstrap/Container'
+import AddTodo from '../components/AddTodo'
 
 const HomePage = () => {
 	return (
 		<Container className="py-3">
 			<h1>Welcome!</h1>
+			<AddTodo />
 		</Container>
 	)
 }
