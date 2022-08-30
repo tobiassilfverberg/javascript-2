5f51351b5f96c29d021818ede0937009a13903c4
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-08-30 13:06:07 +0200

    Now use the new custom hook to get the todo.

--- 06-firebase-todos/src/pages/TodoPage.jsx
+++ 06-firebase-todos/src/pages/TodoPage.jsx
@@ -1,16 +1,33 @@
+import { useState, useEffect } from 'react'
 import Button from 'react-bootstrap/Button'
 import ButtonGroup from 'react-bootstrap/ButtonGroup'
 import Container from 'react-bootstrap/Container'
 import { useParams } from 'react-router-dom'
+import useGetTodo from '../hooks/useGetTodo'
 
 const TodoPage = () => {
 	const { id } = useParams()
+	
+	const [todo, setTodo] = useState()
+	const [loading, setLoading] = useState(true)
+
+	useEffect(() => {
+		const getSnapshot = async () => {
+			setLoading(true)
+			const data = await useGetTodo(id)
+
+			setTodo(data)
+			setLoading(false)
+		}
+
+		getSnapshot()
+	}, [])
 
 	return (
 		<Container className="py-3">
 
 			<div className="d-flex justify-content-between align-items-start mb-3">
-				<h1>{id}</h1>
+				{todo && (<h1>{todo.title}</h1>)}
 
 				<Button onClick={() => {}}>Refresh</Button>
 			</div>
