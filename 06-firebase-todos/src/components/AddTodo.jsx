b8febed0d06db016172f99a98ecd049abe8f01d7
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-08-31 10:28:07 +0200

    Minor changes to 'AddTodo' form.

--- 06-firebase-todos/src/components/AddTodo.jsx
+++ 06-firebase-todos/src/components/AddTodo.jsx
@@ -1,20 +1,19 @@
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
-import { useNavigate } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
 import { collection, addDoc } from 'firebase/firestore'
 import { db } from '../firebase'
 
 const AddTodo = () => {
-	const { register, handleSubmit, formState: { errors } } = useForm()
-	const navigate = useNavigate()
+	const { register, handleSubmit, formState: { errors }, reset } = useForm()
 
 	const onSubmit = async (data) => {
 		await addDoc(collection(db, 'todos'), {
 			title: data.title,
 			completed: false, 
 		})
-		// navigate('/todos')
+
+		reset()
 	}
 	
 	return (
@@ -26,26 +25,15 @@ const AddTodo = () => {
 					{...register('title', {
 						required: 'You give no title?! I no create!',
 						minLength: {
-							value: 2,
+							value: 3,
 							message: 'That\'s no title (enter at least 2 characters)!',
 						},
 					})}
 					type='text'
 					placeholder='Todo Title'
 				/>
-				{errors.name && <div className='invalid'>{errors.title.message}</div>}
+				{errors.title && <div className='invalid'>{errors.title.message}</div>}
 			</Form.Group>
-
-			{/* <Form.Group className='mb-3' controlId='completed'>
-				<Form.Label>Completed</Form.Label>
-				<Form.Control
-					{...register('completed', {
-						required: 'Please specify status of todo',
-					})}
-					type='date'
-				/>
-				{errors.completed && <div className='invalid'>{errors.completed.message}</div>}
-			</Form.Group> */}
 
 			<Button variant='success' type='submit'>
 				Create Todo
