9cbadc74fc750055c91af0f85e0a521e72513693
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-08-31 11:31:04 +0200

    Now use realtime datastreaming for a single todo.

--- 06-firebase-todos/src/hooks/useGetTodo.js
+++ 06-firebase-todos/src/hooks/useGetTodo.js
@@ -1,7 +1,7 @@
-import useGetDocument from './useGetDocument'
+import useStreamDocument from "./useStreamDocument"
 
 const useGetTodo = (id) => {
-	return useGetDocument('todos', id)
+	return useStreamDocument('todos', id)
 }
 
 export default useGetTodo
