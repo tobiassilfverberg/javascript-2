2b27d389c95485ce5b687266320b13a010b2b6d4
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-09-13 14:12:40 +0200

    Added 'FsLightbox' to show images in lightbox-gallery.

--- 07-simple-file-uploads/src/components/ImageCard.jsx
+++ 07-simple-file-uploads/src/components/ImageCard.jsx
@@ -1,13 +1,24 @@
+import { useState } from 'react'
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 import useDeleteImage from '../hooks/useDeleteImage'
+import FsLightbox from 'fslightbox-react'
 
 const ImageCard = ({ image }) => {
 	const deleteImageMutation = useDeleteImage()
+	const [toggler, setToggler] = useState(false)
 
 	return (
+	<>
+		<FsLightbox
+			toggler={toggler}
+			sources={[
+				<img src={image.url} />
+			]}
+		/>
+
 		<Card className={`image-card ${deleteImageMutation.isMutating ? 'mutating' : ''}`}>
 			<Card.Header>
 				<span className="image-filename" title={image.name}>
@@ -25,14 +36,17 @@ const ImageCard = ({ image }) => {
 				</div>
 			</Card.Header>
 
-			<a href={image.url} target="_blank">
-				<Card.Img variant="top" src={image.url} />
-			</a>
+			{/* <a href={image.url} target="_blank"> */}
+				<Card.Img variant="top" src={image.url} onClick={() => {
+					setToggler(!toggler)
+				}}/>
+			{/* </a> */}
 
 			<Card.Footer>
 				{Math.round(image.size / 1024)} kB
 			</Card.Footer>
 		</Card>
+	</>	
 	)
 }
 
