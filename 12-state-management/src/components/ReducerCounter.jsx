a4ad6c3827d39dc97c5ed0541adac8f3b4ee38ce
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-09-27 11:56:55 +0200

    Reset state to initial values.

--- 12-state-management/src/components/ReducerCounter.jsx
+++ 12-state-management/src/components/ReducerCounter.jsx
@@ -3,8 +3,11 @@ import { useReducer } from 'react'
 import Button from 'react-bootstrap/Button'
 
 const ACTIONS = {
+	ADD: 'add',
 	INCREMENT: 'increment',
 	DECREMENT: 'decrement',
+	RESET: 'reset',
+	SUBTRACT: 'subtract',
 }
 
 const initialState = {
@@ -14,15 +17,18 @@ const initialState = {
 const reducer = (state, action) => {
 	switch (action.type) {
 		case ACTIONS.INCREMENT:
-			// increment count
 			return {
 				count: state.count + 1
 			}
+
 		case ACTIONS.DECREMENT:
-			// decrement counter
 			return {
 				count: state.count - 1
 			}
+
+		case ACTIONS.RESET:
+			return initialState
+
 		default:
 			// I not know what to do, error
 			throw new Error(`Unknown action type: ${action.type}`)
@@ -34,11 +40,17 @@ const ReducerCounter = () => {
 
 	return (
 		<div className="reducer-counter">
-			<Button variant="warning" onClick={() => dispatch( { type: ACTIONS.DECREMENT } )}>-</Button>
+			<Button variant="warning" onClick={ () => null }>-10</Button>
+			<Button variant="warning" onClick={ () => null }>-5</Button>
+			<Button variant="warning" onClick={ () => dispatch( { type: ACTIONS.DECREMENT } ) }>-</Button>
 
 			<span className="points">{state.count}</span>
 
-			<Button variant="success" onClick={() => dispatch( { type: ACTIONS.INCREMENT } )}>+</Button>
+			<Button variant="success" onClick={ () => dispatch( { type: ACTIONS.INCREMENT } ) }>+</Button>
+			<Button variant="success" onClick={ () => null }>+5</Button>
+			<Button variant="success" onClick={ () => null }>+10</Button>
+
+			<Button variant="danger" onClick={ () =>  dispatch( { type: ACTIONS.RESET } ) }>🧹</Button>
 		</div>
 	)
 }
