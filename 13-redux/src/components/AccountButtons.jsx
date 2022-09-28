f2a8d0519ef097d3054430b0297114466fd2cf89
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-09-28 10:55:23 +0200

    Use 'useDispatch' to dispatch on deposit/withdraw click.

--- 13-redux/src/components/AccountButtons.jsx
+++ 13-redux/src/components/AccountButtons.jsx
@@ -1,11 +1,14 @@
 import Button from 'react-bootstrap/Button'
 import ButtonGroup from 'react-bootstrap/ButtonGroup'
+import { useDispatch } from 'react-redux'
 
 const AccountButtons = () => {
+	const dispatch = useDispatch()
+
 	return (
 		<ButtonGroup>
-			<Button variant="success" onClick={() => {}}>Deposit</Button>
-			<Button variant="warning" onClick={() => {}}>Withdraw</Button>
+			<Button variant="success" onClick={() => dispatch({ type: 'deposit' })}>Deposit</Button>
+			<Button variant="warning" onClick={() => dispatch({ type: 'withdraw' })}>Withdraw</Button>
 		</ButtonGroup>
 	)
 }
