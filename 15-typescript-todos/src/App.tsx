118543c261dc01f1c5ca5ffe1d6fd94dd55c64f3
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-10-04 09:44:43 +0200

    Boilerplate

---
+++ 15-typescript-todos/src/App.tsx
@@ -0,0 +1,13 @@
+import './assets/scss/App.scss'
+import Container from 'react-bootstrap/Container'
+
+function App() {
+
+	return (
+		<Container id="App" className="py-3">
+			<h1>Todos</h1>
+		</Container>
+	)
+}
+
+export default App
